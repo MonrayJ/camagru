342b0be93374014f926314d6a8e3ad8e49ab507f
Monray JACOBS <mjacobs@e4r25p2.wethinkcode.co.za>
2018-10-17 11:35:21 +0200

    class and child class for database connection and creation

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	include_once 'stuff.php';
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<?php
+			$dbase = new Dbase;
+		?>
+	</body>
+</html>
--- stuff.php
+++ stuff.php
@@ -1,48 +1,50 @@
 <?php
-
-	class Dbase
+	class Create
 	{
-		private $servername;
-		private $username;
-		private $passwoed;
-		private $dbname;
-		private $charset;
+		private $servername = "localhost";
+		private $username = "mjacobs";
+		private $password = "960206";
+		private $dbname = "testdb";
+		private $charset = "utf8mb4";
+
+		public function __construct()
+		{
+			try
+			{
+				$conn = new PDO("mysql:host=$servername", $username, $password);
+				// set the PDO error mode to exception
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$sql = "CREATE DATABASE IF NOT EXISTS $dbase";
+				// use exec() because no results are returned
+				$conn->exec($sql);
+				echo "Database created successfully<br>";
+			}
+			catch(PDOException $e)
+			{
+				echo $sql . "<br>" . $e->getMessage();
+			}
+			$conn = null;
+		}
+	}
+
+	class Dbase extends Create
+	{
+		public function __construct()
+		{}
 
 		public function connect()
 		{
-			$this->servername = "localhost";
-			$this->username = "mjacobs";
-			$this->password = "960206";
-			$this->dbname = "testdb";
-			$this->charset = "utf8mb4";
-
 			try
 			{
 				$dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charse=".$this->charset;
-				$pdo = new PDO($dsn, $this->username, $this->password);
-				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
-				return $pdo;
+				$pdo_conn = new PDO($dsn, $this->username, $this->password);
+				$pdo_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
+				return $pdo_conn;
 			}
 			catch(PDOExeption $e)
 			{
 				echo $sql . "<br>" . $e->getMessage();
 			}
 		}
-	}	
-	try
-	{
-		$conn = new PDO("mysql:host=$servername", $username, $password);
-		// set the PDO error mode to exception
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "CREATE DATABASE IF NOT EXISTS $dbase";
-		// use exec() because no results are returned
-		$conn->exec($sql);
-		echo "Database created successfully<br>";
 	}
-	catch(PDOException $e)
-	{
-		echo $sql . "<br>" . $e->getMessage();
-	}
-
-	$conn = null;
 ?>
