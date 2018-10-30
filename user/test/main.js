923a0253a8a6a2fb47e1d6a603d0b8ab98fe45b7
Monray JACOBS <mjacobs@e4r25p2.wethinkcode.co.za>
2018-10-30 12:18:17 +0200

    busy with modal

---
+++ user/test/main.js
@@ -0,0 +1,6 @@
+// Get model element
+var modal = document.getElementById('simpleModel');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+// Get close butten
+var closeBtn = document.getElementById
